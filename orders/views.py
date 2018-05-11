6396fd165fd562ab5a0bb872e8a303cafde7ea31
myprojectlist <vikasvasireddy264@gmail.com>
2018-05-11 12:21:52 +0530

    second commit

---
+++ orders/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+from .tasks import order_created
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+
+# Create your views here.
+
+def order_create(request):
+	cart = Cart(request)
+	if request.method == 'POST':
+		form = OrderCreateForm(request.POST)
+		if form.is_valid():
+			order = form.save()
+			for item in cart:
+				OrderItem.objects.create(order=order,product=item['product'],price=item['price'],quantity=item['quantity'])
+# clear the cart
+			cart.clear()
+			order_created.delay(order.id)
+			request.session['order_id'] = order.id # redirect to the payment
+			return redirect(reverse('payment:process')) 
+	else:
+		form = OrderCreateForm()
+	return render(request,'orders/order/create.html',{'cart': cart, 'form': form})		
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+	order = get_object_or_404(Order, id=order_id)
+	return render(request,'admin/orders/order/detail.html',{'order': order})
